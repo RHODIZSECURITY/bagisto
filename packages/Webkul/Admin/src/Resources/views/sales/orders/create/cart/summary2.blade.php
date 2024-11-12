38f6896c05c65fb7915d5bec19e6169239f3171c
reiarseni <reiarseni@gmail.com>
2024-11-11 20:40:06 -0500

    [update] Adicionado Taxes a ordenes de ventas de tienda
    - Quitada la parte del cupon

--- packages/Webkul/Admin/src/Resources/views/sales/orders/create/cart/summary2.blade.php
+++ packages/Webkul/Admin/src/Resources/views/sales/orders/create/cart/summary2.blade.php
@@ -2,8 +2,6 @@
 
 <v-cart-summary
     :cart="cart"
-    @coupon-applied="setCart"
-    @coupon-removed="setCart"
 ></v-cart-summary>
 
 {!! view_render_event('bagisto.admin.sales.order.create.cart.summary.after') !!}
@@ -77,7 +75,6 @@
 
                     {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.sub_total.after') !!}
 
-
                     <!-- Taxes -->
                     {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.tax.before') !!}
 
@@ -97,58 +94,60 @@
 
                     {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.tax.after') !!}
 
-                    <!-- Shipping Rates -->
-                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.delivery_charges.before') !!}
 
-                    <template v-if="displayTax.shipping == 'including_tax'">
-                        <div class="row grid grid-cols-2 grid-rows-1 justify-between gap-4 text-right">
-                            <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                                @lang('admin::app.sales.orders.create.cart.summary.shipping-amount')
+                    <!-- Taxes -->
+                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.tax.before') !!}
+
+                    <div
+                        class="flex justify-between text-right"
+                        v-if="! cart.tax_total"
+                    >
+                        <p class="text-base font-medium text-gray-600 dark:text-gray-300">
+                            @lang('shop::app.checkout.cart.summary.tax')
+                        </p>
+
+                        <p class="text-base font-medium text-gray-600 dark:text-gray-300">
+                            @{{ cart.formatted_tax_total }}
+                        </p>
+                    </div>
+
+                    <div
+                        class="flex flex-col gap-2 border-y py-2"
+                        v-else
+                    >
+                        <div
+                            class="flex cursor-pointer justify-between text-right dark:text-white"
+                            @click="cart.show_taxes = ! cart.show_taxes"
+                        >
+                            <p class="text font-semibold dark:text-white">
+                                @lang('shop::app.checkout.cart.summary.tax')
                             </p>
 
-                            <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                                @{{ cart.formatted_shipping_amount_incl_tax }}
-                            </p>
-                        </div>
-                    </template>
-
-                    <template v-else-if="displayTax.shipping == 'both'">
-                        <div class="row grid grid-cols-2 grid-rows-1 justify-between gap-4 text-right">
-                            <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                                @lang('admin::app.sales.orders.create.cart.summary.shipping-amount-excl-tax')
-                            </p>
-
-                            <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                                @{{ cart.formatted_shipping_amount }}
-                            </p>
-                        </div>
-
-                        <div class="row grid grid-cols-2 grid-rows-1 justify-between gap-4 text-right">
-                            <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                                @lang('admin::app.sales.orders.create.cart.summary.shipping-amount-incl-tax')
-                            </p>
-
-                            <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                                @{{ cart.formatted_shipping_amount_incl_tax }}
+                            <p class="flex items-center gap-1 text-base font-medium max-md:font-medium dark:text-white">
+                                @{{ cart.formatted_tax_total }}
                             </p>
                         </div>
-                    </template>
 
-                    <template v-else>
-                        <div class="row grid grid-cols-2 grid-rows-1 justify-between gap-4 text-right">
-                            <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                                @lang('admin::app.sales.orders.create.cart.summary.shipping-amount')
-                            </p>
+                        <div
+                            class="flex flex-col gap-1 dark:text-white"
+                            v-show="cart.show_taxes"
+                        >
+                            <div
+                                class="flex justify-between gap-1 text-right"
+                                v-for="(amount, index) in cart.applied_taxes"
+                            >
+                                <p class="text-sm max-md:text-sm max-md:font-normal">
+                                    @{{ index }}
+                                </p>
 
-                            <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                                @{{ cart.formatted_shipping_amount }}
-                            </p>
+                                <p class="text-sm font-medium max-md:text-sm max-md:font-medium">
+                                    @{{ amount }}
+                                </p>
+                            </div>
                         </div>
-                    </template>
+                    </div>
 
-
-
-                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.delivery_charges.after') !!}
+                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.tax.after') !!}
 
                     <!-- Discount -->
                     {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.discount_amount.before') !!}
@@ -167,43 +166,6 @@
                     </div>
 
                     {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.discount_amount.after') !!}
-
-
-                    <!-- Discount -->
-                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.coupon.before') !!}
-
-                    <div class="row grid grid-cols-2 grid-rows-1 justify-items-end gap-4 text-right">
-                        <p class="text-base font-medium text-gray-600 dark:text-gray-300">
-                            @lang('admin::app.sales.orders.create.cart.summary.apply-coupon')
-                        </p>
-
-                        <template v-if="cart.coupon_code">
-                            <p class="flex items-center gap-1 text-base font-medium text-green-600">
-                                @{{ cart.coupon_code }}
-
-                                <span
-                                    class="icon-cancel cursor-pointer text-2xl"
-                                    @click="destroyCoupon"
-                                >
-                                </span>
-                            </p>
-                        </template>
-
-                        <template v-else>
-                            <p class="text-base font-medium text-gray-600">
-                                <span
-                                    class="cursor-pointer text-blue-600"
-                                    @click="$refs.couponModel.open()"
-                                >
-                                    @lang('admin::app.sales.orders.create.cart.summary.apply-coupon')
-                                </span>
-                            </p>
-                        </template>
-                    </div>
-
-                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.coupon.after') !!}
-
-                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.coupon.after') !!}
 
                     <!-- Cart Grand Total -->
                     {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.grand_total.before') !!}
@@ -232,52 +194,6 @@
                     @click="placeOrder"
                 />
             </div>
-
-            <!-- Apply Coupon Form -->
-            <x-admin::form
-                v-slot="{ meta, errors, handleSubmit }"
-                as="div"
-            >
-                <!-- Apply coupon form -->
-                <form @submit="handleSubmit($event, applyCoupon)">
-                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.coupon_form_controls.before') !!}
-
-                    <!-- Apply coupon modal -->
-                    <x-admin::modal ref="couponModel">
-                        <!-- Modal Header -->
-                        <x-slot:header class="dark:text-white">
-                            @lang('admin::app.sales.orders.create.cart.summary.apply-coupon')
-                        </x-slot>
-
-                        <!-- Modal Content -->
-                        <x-slot:content>
-                            <x-admin::form.control-group class="!mb-0">
-                                <x-admin::form.control-group.control
-                                    type="text"
-                                    name="code"
-                                    rules="required"
-                                    :placeholder="trans('admin::app.sales.orders.create.cart.summary.enter-your-code')"
-                                />
-
-                                <x-admin::form.control-group.error control-name="code" />
-                            </x-admin::form.control-group>
-                        </x-slot>
-
-                        <!-- Modal Footer -->
-                        <x-slot:footer>
-                            <!-- Save Button -->
-                            <x-admin::button
-                                class="primary-button"
-                                :title="trans('admin::app.sales.orders.create.cart.summary.apply-coupon')"
-                                ::loading="isLoading"
-                                ::disabled="isLoading"
-                            />
-                        </x-slot>
-                    </x-admin::modal>
-
-                    {!! view_render_event('bagisto.admin.sales.order.create.left_component.summary.coupon_form_controls.after') !!}
-                </form>
-            </x-admin::form>
         </div>
     </script>
 
@@ -293,8 +209,6 @@
                         prices: "{{ core()->getConfigData('sales.taxes.shopping_cart.display_prices') }}",
 
                         subtotal: "{{ core()->getConfigData('sales.taxes.shopping_cart.display_subtotal') }}",
-
-                        shipping: "{{ core()->getConfigData('sales.taxes.shopping_cart.display_shipping_amount') }}",
                     },
 
                     isLoading: false,
@@ -304,50 +218,6 @@
             },
 
             methods: {
-                applyCoupon(params, { resetForm }) {
-                    this.isLoading = true;
-
-                    this.$axios.post("{{ route('admin.sales.cart.store_coupon', $cart->id) }}", params)
-                        .then((response) => {
-                            this.isLoading = false;
-
-                            this.$emit('coupon-applied', response.data.data);
-
-                            this.$emitter.emit('add-flash', { type: 'success', message: response.data.message });
-
-                            this.$refs.couponModel.toggle();
-
-                            resetForm();
-                        })
-                        .catch((error) => {
-                            this.isLoading = false;
-
-                            this.$refs.couponModel.toggle();
-
-                            if ([400, 422].includes(error.response.request.status)) {
-                                this.$emitter.emit('add-flash', { type: 'warning', message: error.response.data.message });
-
-                                resetForm();
-
-                                return;
-                            }
-
-                            this.$emitter.emit('add-flash', { type: 'error', message: error.response.data.message });
-                        });
-                },
-
-                destroyCoupon() {
-                    this.$axios.delete("{{ route('admin.sales.cart.remove_coupon', $cart->id) }}", {
-                            '_token': "{{ csrf_token() }}"
-                        })
-                        .then((response) => {
-                            this.$emit('coupon-removed', response.data.data);
-
-                            this.$emitter.emit('add-flash', { type: 'success', message: response.data.message });
-                        })
-                        .catch(error => console.log(error));
-                },
-
                 placeOrder() {
                     this.isPlacingOrder = true;
 
